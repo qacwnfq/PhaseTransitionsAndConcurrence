4aec42ba49e652a3265c9d70756d2e564466fd26
Fredrik <jadebeck@psyware.de>
2016-07-04 00:45:36 +0900

    Added test case for zeeman

--- test/test_quantumMechanics.py
+++ test/test_quantumMechanics.py
@@ -1,7 +1,10 @@
-from code.quantumMechanics import state, ketBra
+import math
+import numpy as np
+
+from code.quantumMechanics import state, ketBra, zeeman
 
 
-def test_state():
+def test_states_and_ketBra():
     # 1) case
     s1 = state(['u', 'd', 'd', 'u'])
     s2 = state(['u', 'u', 'd', 'u'])
@@ -9,11 +12,12 @@ def test_state():
     actual = s1.scalar_product(s2)
     assert(actual == expected)
 
-    expected = 1
     actual = s2.scalar_product(s2)
-    assert(actual == expected)
     expected = 1
+    assert(actual == expected)
+
     actual = s1.scalar_product(s1)
+    expected = 1
     assert(actual == expected)
 
     actual = s1.tensor_product(s2)
@@ -31,14 +35,9 @@ def test_state():
 
     ketBra1 = ketBra(s1, s2)
     ketBra2 = ketBra(s2, s1)
-    print(ketBra1)
-    print(ketBra2)
     actual = ketBra1.multiply_with(ketBra2)
     actual = 0 if actual.amplitude == 0 else actual
     expected = ketBra(s1, s1)
-    print("actual")
-    print(actual)
-    print(expected)
     assert(actual == expected)
 
     # 2) case
@@ -60,4 +59,58 @@ def test_state():
     s2 = state([])
     expected = 1
     actual = s1.scalar_product(s2)
+    assert(actual == expected)
+
+    # 5) case
+    k1 = state(['u', 'd'])
+    k2 = state(['d', 'u'])
+    s1 = k1 + k2
+    s2 = k2 + k1
+
+    actual = s1.scalar_product(k2)
+    expected = 1./math.sqrt(2)
+    assert(actual == expected)
+
+    actual = s1.scalar_product(s1)
+    expected = 1
+    np.testing.assert_almost_equal(actual, expected)
+
+    actual = s1.scalar_product(s2)
+    expected = 1
+    np.testing.assert_almost_equal(actual, expected)
+
+
+def test_zeeman():
+    # 1) case
+    actual = zeeman(2, 1, 1)
+    expected = state(['u', 'u'])
+    assert(actual == expected)
+    assert(actual.state == expected.state)
+
+    # 2) case
+    actual = zeeman(2, 1, 0)
+    expected = state(['d', 'u']) + state(['u', 'd'])
+    assert(actual == expected)
+    assert(actual.state == expected.state)
+    np.testing.assert_almost_equal(actual.nm, expected.nm)
+
+    # 3) case
+    actual = zeeman(2, 1, -1)
+    expected = state(['d', 'd'])
+    assert(actual == expected)
+    assert(actual.state == expected.state)
+
+    # 4) case
+    actual = zeeman(3, 1.5, -1.5)
+    expected = state(['d', 'd', 'd'])
+    assert(actual == expected)
+
+    # 5) case
+    actual = zeeman(4, 2, 0)
+    expected = (state(['u', 'u', 'd', 'd']) +
+                state(['d', 'd', 'u', 'u']) +
+                state(['u', 'd', 'd', 'u']) +
+                state(['d', 'u', 'u', 'd']) +
+                state(['u', 'd', 'u', 'd']) +
+                state(['d', 'u', 'd', 'u']))
     assert(actual == expected)
