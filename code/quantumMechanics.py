28dbf9908e119a7c6bc586d2ffbea26732eaec7d
Fredrik <jadebeck@psyware.de>
2016-07-04 13:03:28 +0900

    One term for partial traces finished.

--- code/quantumMechanics.py
+++ code/quantumMechanics.py
@@ -4,6 +4,7 @@
 # density matrices and their partial traces.
 from itertools import permutations
 import math
+import numpy as np
 
 from code.quantumAnnealing import gen_m
 
@@ -14,15 +15,82 @@ class dm:
         The density matrix is a dictionary where they keys are
         the ketbras and the values are the amplitudes."""
         self.rho = {}
+        self.N = N
         S = N/2
-        for m in gen_m(N):
-            for n in gen_m(N):
-                # Todo figure out mapping of rho onto zeeman
-                self.rho[ketBra(zeeman(N, S, n), zeeman(N, S, m))] = rho
+        ms = gen_m(N)
+        for m in range(len(ms)):
+            for n in range(len(ms)):
+                self.rho[(ketBra(zeeman(N, S, ms[n]),
+                                 zeeman(N, S, ms[m])))] = rho[n][m]
+
+    def nparray(self):
+        result = np.zeros(shape=(self.N+1, self.N+1))
+        ms = gen_m(self.N)
+        for m in range(len(ms)):
+            for n in range(len(ms)):
+                result[n][m] = (self.rho[
+                    (ketBra(zeeman(self.N, self.N/2, ms[n]),
+                            zeeman(self.N, self.N/2, ms[m])))])
+        return result
 
     def ptrace(self, k):
         """traces out the kth spin"""
-        pass
+        result = {}
+        ms = gen_m(self.N-1)
+        for m in range(len(ms)):
+            for n in range(len(ms)):
+                result[(ketBra(zeeman(self.N-1, (self.N-1)/2, ms[n]),
+                               zeeman(self.N-1, (self.N-1)/2, ms[m])))] = 0
+        # calculates which terms add up
+        for key, value in self.rho.items():
+            # up
+            newKet = None
+            newKetFactor = 1
+            newBra = None
+            newBraFactor = 1
+            for i in range(len(key.ket.state)):
+                if key.ket.state[i][k] == 'd':
+                    if newKet is None:
+                        newstate = key.ket.state[i]
+                        del newstate[k]
+                        newKet = state(newstate)
+                    else:
+                        newstate = key.ket.state[i]
+                        del newstate[k]
+                        newKet += state(newstate)
+            for i in range(len(key.bra.state)):
+                if key.bra.state[i][k] == 'd':
+                    if newBra is None:
+                        newstate = key.bra.state[i]
+                        del newstate[k]
+                        newBra = state(newstate)
+                    else:
+                        newstate = key.bra.state[i]
+                        del newstate[k]
+                        newBra += state(newstate)
+            if newKet is not None and newBra is not None:
+                newKetFactor = newKet.nm/key.ket.nm
+                newBraFactor = newBra.nm/key.bra.nm
+                print(newKetFactor)
+                print(newBraFactor)
+                print(ketBra(newKet, newBra))
+                result[(ketBra(newKet, newBra))] += (value *
+                                                     newKetFactor *
+                                                     newBraFactor)
+        # updates N
+        self.N -= 1
+        self.rho = result
+        return self
+
+    def __str__(self):
+        s = ""
+        for i, v in self.rho.items():
+            if math.isclose(self.rho[i], 0):
+                continue
+            else:
+                s += (" + " + str(v) + "(" +
+                      str(i.amplitude) + str(i) + ")")
+        return s
 
 
 class state:
@@ -86,10 +154,11 @@ class state:
 
 
 class ketBra:
-    def __init__(self, ket, bra, amplitude=1):
+    def __init__(self, ket, bra, amplitude=None):
         self.ket = ket
         self.bra = bra
-        self.amplitude = amplitude
+        if amplitude is None:
+            self.amplitude = 1./(self.ket.nm*self.bra.nm)
 
     def multiply_with(self, other):
         result = self.ket.tensor_product(other.bra)
@@ -106,11 +175,17 @@ class ketBra:
             result = True
         return result
 
+    def __hash__(self):
+        h = self.ket.state[0] + self.bra.state[0]
+        t = tuple(h)
+        j = hash(t)
+        return j
+
 
 def zeeman(N, S, m):
     """returns a ket in the zeeman basis for N spins with total Spin
     S and magnetic order m"""
-    # this function will only do the special case for now for simplicity
+    # this function will only do the S=N/2 case for now.
     assert(S == N/2)
     s = state(N*['u'])
     n = int(S - m)
@@ -121,17 +196,10 @@ def zeeman(N, S, m):
         if p not in ps:
             ps.append(p)
     zstate = state(ps[0])
-    # for p in ps:
-    #    zstate += state(p)
     for i in range(1, len(ps)):
         zstate += state(ps[i])
-
-    # print(str(1./zstate.nm) + " " + str(zstate))
-    # print("")
     return zstate
 
 if __name__ == "__main__":
-    zeeman(3, 1.5, 1.5)
-    zeeman(3, 1.5, .5)
-    zeeman(3, 1.5, -.5)
-    zeeman(3, 1.5, -1.5)
+    print(gen_m(4))
+    pass
