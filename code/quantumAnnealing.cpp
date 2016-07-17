479932e31704992999b0f8118618ec1010b0aee4
Fredrik <jadebeck@psyware.de>
2016-07-17 16:46:15 +0900

    Added H0, Vtf, ... and lambdaOne()

--- code/quantumAnnealing.cpp
+++ code/quantumAnnealing.cpp
@@ -6,10 +6,12 @@
 #include <assert.h>
 #include <cmath>
 #include <Eigen/Core>
+#include <Eigen/Eigenvalues>
 #include <unsupported/Eigen/MatrixFunctions>
 #include <fstream>
 #include <iostream>
 #include <set>
+#include <sstream>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -83,12 +85,90 @@ int cardaniac(const double& A, const double& B, const double& C, const double& D
 Matrix<double, Dynamic, Dynamic> H0(const int& N, const int& p)
 {
   assert(N > 0);
-  double S = double(N)/2;
-  Matrix<double, Dynamic, Dynamic> H0 = Sz(N).pow(p);
-  H0 *= -N/S;
+  double S = double(N)/2.;
+  Matrix<double, Dynamic, Dynamic> H0 = Sz(N);
+  H0 /= S;
+  // Since the matrix is diagonal, the power is taken like this.
+  for(int i=0; i<N+1; ++i)
+    H0(i, i) = std::pow(H0(i, i), p);
+  H0 *= -N;
   return H0;
 }
 
-int main()
+Matrix<double, Dynamic, Dynamic> Vtf(const int& N)
 {
+  assert(N > 0);
+  double S = double(N)/2;
+  Matrix<double, Dynamic, Dynamic> Vtf = Sx(N);
+  Vtf /= S;
+  Vtf *= -N;
+  return Vtf;
+}
+
+Matrix<double, Dynamic, Dynamic> Vaff(const int& N)
+{
+  assert(N > 0);
+  double S = double(N)/2;
+  Matrix<double, Dynamic, Dynamic> Vaff = Sx(N);
+  Vaff /= S;
+  // TODO pow doesnt work
+  Vaff = Vaff.pow(2);
+  Vaff *= N;
+  return Vaff;
+}
+
+Matrix<double, Dynamic, Dynamic> H0plusVtf(const int& N, const double& s, const int& p)
+{
+  return s*H0(N, p) + (1-s)*Vtf(N);
+}
+
+Matrix<double, Dynamic, Dynamic> H0plusVaffplusVtf(const int& N, const double& s, const double& l, const int& p)
+{
+  return s*l*H0(N, p) + s*(1-l)*Vaff(N) + (1-s)*Vtf(N);
+}
+
+SelfAdjointEigenSolver<Matrix<double, Dynamic, Dynamic>> diagonalize(Matrix<double, Dynamic, Dynamic> H)
+{
+  SelfAdjointEigenSolver<Matrix<double, Dynamic, Dynamic>> es;
+  es.compute(H);
+  return es;
+}
+
+void lambdaOne(const int& p)
+{
+  //Calculates the groundstate energy for lambda=1
+  std::vector<double> s_list = linspace(0, 1, 101);
+  std::vector<int> N_list = {2, 4, 8, 16, 32, 64, 128, 256, 512};
+  std::vector<std::vector<double>> energies;
+  for(int N: N_list)
+  {
+    std::cout << "Calculating " << N << " Spins." << std::endl;
+    std::vector<double> energy;
+    for(double s: s_list)
+    {
+      SelfAdjointEigenSolver<Matrix<double, Dynamic, Dynamic> > es;
+      es = diagonalize(H0plusVtf(N, s, p));
+      double ev = es.eigenvalues()(0);
+      energy.push_back(ev/N);
+    }
+    energies.push_back(energy);
+  }
+  std::cout << "Done." << std::endl;
+  Gnuplot gp("Energy per spin");
+  std::ostringstream s;
+  s << "Energy per Spin ";
+  auto title = s.str();
+  gp.set_title(title);
+  gp.set_xlabel("s");
+  for(int i=0; i<N_list.size(); ++i)
+  {
+    std::ostringstream s2;
+    s2 << N_list[i] << " Spins";
+    gp.set_style("points").plot_xy(s_list, energies[i], s2.str());
+  }
+  gp.unset_smooth();
+  gp.showonscreen();
+  std::cout << "Press Enter to exit." << std::endl;
+  double e;
+  std::cin >> e;
 }
