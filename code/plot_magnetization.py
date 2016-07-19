1e23ec2e5e0d72ed173177d784e4a4192127bdac
Fredrik <jadebeck@psyware.de>
2016-07-19 13:17:26 +0900

    plot_magnetization now plots several cases in one plot.

--- code/plot_magnetization.py
+++ code/plot_magnetization.py
@@ -1,21 +1,30 @@
 from matplotlib import pyplot as plt
 import csv
-file = "../results/magnetization/MagnetizationLambda0.8p5.csv"
+import numpy as np
 
-spamReader = csv.reader(open(file), delimiter=',', quotechar='|')
-mx = []
-mz = []
-s = []
-next(spamReader)
-next(spamReader)
-for row in spamReader:
-    s.append(row[0])
-    mx.append(row[1])
-    mz.append(row[2])
+l_list = np.linspace(0, 1, 11)
+for l in l_list:
+    if l == 0:
+        continue
+    if l == 1:
+        l = str(1)
+    file = "../results/magnetization/MagnetizationLambda" + str(l) + "p5.csv"
+    spamReader = csv.reader(open(file), delimiter=',', quotechar='|')
+    mx = []
+    mz = []
+    s = []
+    next(spamReader)
+    next(spamReader)
+    for row in spamReader:
+        s.append(row[0])
+        mx.append(row[1])
+        mz.append(row[2])
 
-plt.plot(s, mx, "mx")
-plt.plot(s, mz, "")
-plt.xlabel("s")
-plt.ylabel("mx")
-plt.title("lambda 0.8")
+    plt.grid(True)
+    plt.plot(s, mx, marker='.', ls='None', label="mx, l=" + str(l))
+    # plt.plot(s, mz, marker='.', ls='None', label="mz, l=" + str(l))
+    plt.xlabel("s")
+
+plt.title("Magnetization for p=5")
+plt.legend()
 plt.show()
