f4d847663e402f8c3b055b32721d3d5664ddf5d5
Fredrik <jadebeck@psyware.de>
2016-07-20 17:34:36 +0900

    Code review fixes for runQuantumAnnealing.cpp

--- code/runQuantumAnnealing.cpp
+++ code/runQuantumAnnealing.cpp
@@ -7,14 +7,11 @@
 int main(int argc, char* argv[])
 {
   int p = 5;
+  // This program accepts one parameter for p
   if(argc == 2)
   {
     p = std::atoi(argv[1]);
   }
-  // runs groundstate energy calculation for p=5
-  //　lambdaOne(5);
-  // lambdaOneConcurrence(5);
-  // lambdaNotOne(5);
   std::cout << "Running for p=" << std::to_string(p) << std::endl;
   lambdaNotOneConcurrence(p);
 }
